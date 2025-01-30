987154975f4a92ea2a18e59e71891a41de09f274
Fernando Mata <fernando.mata@momofactor.com>
2025-01-30 12:46:53 -0600

    Added lambda trigger to lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -242,7 +242,7 @@ resource "aws_lambda_function" "send_email" {
   ]
 }
 
-resource "aws_lambda_event_source_mapping" "report_batches_sqs_trigger" {
+resource "aws_lambda_event_source_mapping" "send_email_sqs_trigger" {
   event_source_arn = aws_sqs_queue.send_email_queue.arn
   function_name    = aws_lambda_function.send_email.function_name
   batch_size       = 1
